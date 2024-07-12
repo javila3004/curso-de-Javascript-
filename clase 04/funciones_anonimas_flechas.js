32b382297574cc7d7a42d85f619df57d315d0e43
javila3004 <104645424+javila3004@users.noreply.github.com>
2024-07-12 13:30:27 -0300

    se borraron las clases

--- clase 04/funciones_anonimas_flechas.js
+++
@@ -1,8 +0,0 @@
-
-const  saludar =function (nombre1) {
-    alert("Hola"+ nombre1 + "soy una funcion")
-   
-}
-
-//funcione de flcha
-const mostrarDia = (dia) => alert(" hoy es " + dia);
--- clase 04/scope.js
+++
@@ -1,6 +0,0 @@
-let resultado2=0;
-function doble(numero){
-    resultado2 = numero * 2;
-}
-doble(20);
-alert("Elresultado es: " +resultado2)
